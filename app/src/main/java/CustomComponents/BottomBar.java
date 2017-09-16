8a633dd00183b00dfa192c11b819485a0cf75ae6
Endrawan <nadirahmat@gmail.com>
2017-09-16 23:46:09 +0700

    added shared element between home fragment and search fragment, new home scrolling behavior #8

--- app/src/main/java/CustomComponents/BottomBar.java
+++ app/src/main/java/CustomComponents/BottomBar.java
@@ -1,13 +1,19 @@
 package CustomComponents;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.content.Context;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.view.View;
 
 /**
  * Endrawan made this on 2017-09-15.
  */
 public class BottomBar extends com.roughike.bottombar.BottomBar {
+    protected boolean showed = true;
+    private myAnimatorListener animatorListener = new myAnimatorListener();
+
     public BottomBar(Context context) {
         super(context);
     }
@@ -25,10 +31,25 @@ public class BottomBar extends com.roughike.bottombar.BottomBar {
     }
 
     public void animatePutToBottom(int duration) {
-        this.animate().translationY(this.getHeight()).setDuration(duration);
+        showed = false;
+        this.animate().translationY(this.getHeight()).setDuration(duration).setListener(animatorListener);
     }
 
     public void animatePutToNormal(int duration) {
-        this.animate().translationY(0).setDuration(duration);
+        this.setVisibility(View.VISIBLE);
+        showed = true;
+        this.animate().translationY(0).setDuration(duration).setListener(animatorListener);
     }
+
+    private class myAnimatorListener extends AnimatorListenerAdapter {
+        @Override
+        public void onAnimationEnd(Animator animation) {
+            super.onAnimationEnd(animation);
+            if (showed)
+                BottomBar.this.setVisibility(View.VISIBLE);
+             else
+                BottomBar.this.setVisibility(GONE);
+        }
+    }
+
 }
