0cc079bc783edd7778be1a961ce06ec0f06c416c
Endrawan <nadirahmat@gmail.com>
2017-09-27 16:27:13 +0700

    added anonymous service(user can buy without sign up) #17

---
+++ app/src/main/java/CustomComponents/Fragment.java
@@ -0,0 +1,9 @@
+package CustomComponents;
+
+/**
+ * Endrawan made this on 2017-09-28.
+ */
+public class Fragment extends android.support.v4.app.Fragment {
+    protected int position, iconPosition;
+    protected String title;
+}
