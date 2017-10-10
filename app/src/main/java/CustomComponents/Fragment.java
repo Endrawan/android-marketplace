fc5e90178e3e47024eb220fd3906ec7fc6ba90bc
Endrawan <nadirahmat@gmail.com>
2017-10-10 15:21:54 +0700

    Added More Products in Main Menu #26

--- app/src/main/java/CustomComponents/Fragment.java
+++ app/src/main/java/CustomComponents/Fragment.java
@@ -6,4 +6,28 @@ package CustomComponents;
 public class Fragment extends android.support.v4.app.Fragment {
     protected int position, iconPosition;
     protected String title;
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
+
+    public int getIconPosition() {
+        return iconPosition;
+    }
+
+    public void setIconPosition(int iconPosition) {
+        this.iconPosition = iconPosition;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
